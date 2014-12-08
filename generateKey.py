e5a375f27239123e0595f54b8fc814d096fd695b
Chris Taylor <christaylortf@users.noreply.github.com>
2014-12-08 11:49:23 +0000

    Update generateKey.py

--- generateKey.py
+++ generateKey.py
@@ -1,3 +1,16 @@
-import paperclip, sys, random
+import sys, collections
+from random import randrange
 
-letters = [0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"]
+
+#random number gen 
+num = randrange(37)
+print num
+
+#generate key
+key = collections.deque(letters)
+key.rotate(num)
+
+print letters
+print "Shift by " + key[0]
+print key
